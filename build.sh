793d3b2a38ae8e722f8436acfd8d1f55ebf89549
alex31bel <alex@31bel.ru>
2022-07-08 12:55:37 +0300

    Меняем репозиторий на Testing

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 lb config -d sid \
 	--debian-installer live \
-	--debian-installer-distribution sid \
+	--debian-installer-distribution testing \
 	--debian-installer-gui true \
 	--archive-areas "main contrib non-free" \
 	--debootstrap-options "--variant=minbase" \
          --iso-publisher 'Custom Debian Builds; https://github.com/alex31bel/Debian-Live-Builds; alex@31bel.ru' \
-         --iso-volume debian-sid-gnome \
+         --iso-volume debian-testing-gnome \
          --clean \
          --color \
-         --image-name debian-sid-gnome.beta
+         --image-name debian-testing-gnome.beta
           
 sudo lb build 
